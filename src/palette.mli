788829dc6c3a751db305ff588a70dd95c42abc83
essa237 <wahvanessa22@gmail.com>
2025-03-29 07:43:26 +0100

    added a d to update to correct the error in interface file

--- src/palette.mli
+++ src/palette.mli
@@ -70,5 +70,5 @@ val index_to_rgb: t -> int -> int32
 val circle_palette: t -> int -> int32
 (**[circle_palette pal offset] returns a new palette with entries rotated to offset*)
 
-val update_entry: t -> int -> int32
-(** [update_entry pal index new_color] checks for the index then returns a new palette with the entry at [index] updated to [new_color]. *)
+val updated_entry : t -> int -> int32 -> t
+(** [updated_entry pal index new_color] checks for the index then returns a new palette with the entry at [index] updated to [new_color]. *)
