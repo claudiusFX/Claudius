de92c24cbbe4e53228189581dbb7047d402ddc1a
essa237 <wahvanessa22@gmail.com>
2025-04-04 14:18:37 +0100

    written test to test update palette

--- test/test_screen.ml
+++ test/test_screen.ml
@@ -20,11 +20,24 @@ let test_fail_invalid_dimensions _ =
   assert_raises (Invalid_argument "Invalid height") (fun _ -> Screen.create 10 (-10) 2 palette);
   assert_raises (Invalid_argument "Invalid width") (fun _ -> Screen.create (-10) 10 2 palette)
 
+let test_update_palette _ =
+  let initial_palette = Palette.generate_mono_palette 2 in
+  let screen = Screen.create 640 480 2 initial_palette in
+  Screen.clear_dirty screen;
+  assert_equal ~msg:"Dirty flag should be false after clearing" false (Screen.is_dirty screen);
+  let new_palette = Palette.generate_plasma_palette 2 in
+   Screen.update_palette screen new_palette;
+   assert_equal ~msg:"Palette should be updated" new_palette (Screen.palette screen);
+   assert_equal ~msg:"Dirty flag should be true after update" true (Screen.is_dirty screen);
+   Screen.clear_dirty screen;
+   assert_equal ~msg:"Dirty flag should be cleared" false (Screen.is_dirty screen)
+
 let suite =
   "Screen tests" >::: [
     "Test simple screen set up" >:: test_basic_screen_creation ;
     "Test fail with invalid scale" >:: test_fail_invalid_scale ;
     "Test fail with invalid dimensions" >:: test_fail_invalid_dimensions ;
+    "Test update palette">:: test_update_palette ;
   ]
 
 let () =
