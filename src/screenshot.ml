00eae072144eb2352977005caa7bb3539b7bc4c7
Shreya Pawaskar <shreya.pawaskar.main@gmail.com>
2025-04-09 19:06:58 +0000

    flattening changed to 8 bits/byte from color_depth

--- src/screenshot.ml
+++ src/screenshot.ml
@@ -31,29 +31,11 @@ let pad_palette_to_power_of_two (colors : ColorTable.t) : ColorTable.t =
 let save_screenshot (fb : Framebuffer.t) (palette : Palette.t) =
   let width = Array.length fb.data.(0) in
   let height = Array.length fb.data in
-  Printf.printf "Framebuffer dimensions: %d x %d\n" width height;   (*Debug*)
+
   let size = width * height in
 
-  let nulls_replaced = ref 0 in
-  let pixels =
-    Bytes.init size (fun idx ->
-      let x = idx mod width in
-      let y = idx / width in
-      let v = fb.data.(y).(x) in
-      if v < 0 || v > 255 then
-        failwith (Printf.sprintf "Framebuffer value %d out of byte range at (%d,%d)" v x y);
-      if v = 0 then incr nulls_replaced;
-      let max_index = Palette.size palette - 1 in
-      let safe_v = 
-        if v = 0 then 1
-        else if v > max_index then max_index
-        else v in
-      Char.chr safe_v
-    )
-  in
-  Printf.printf "Replaced %d null pixels with index 1\n%!" !nulls_replaced;
-
-  let colors = palette
+  let colors =
+    palette
     |> color_table_of_palette
     |> pad_palette_to_power_of_two
   in
@@ -61,40 +43,38 @@ let save_screenshot (fb : Framebuffer.t) (palette : Palette.t) =
   let color_depth =
     let len = Array.length colors in
     let rec bits_needed n b = if n <= 1 then b else bits_needed (n / 2) (b + 1) in
-    min 8 (max 2 (bits_needed (len-1) 1))
+    min 8 (max 2 (bits_needed (len - 1) 1))
   in
 
-  let palette_len = Array.length colors in
-  let max_allowed_color_depth =
-    let rec bits_needed n b = if n <= 1 then b else bits_needed (n / 2) (b + 1) in
-    bits_needed (palette_len - 1) 1
+  let nulls_replaced = ref 0 in
+  let pixels =
+    List.init size (fun idx ->
+      let x = idx mod width in
+      let y = idx / width in
+      let v = fb.data.(y).(x) in
+      if v < 0 || v > 255 then
+        failwith (Printf.sprintf "Framebuffer value %d out of byte range at (%d,%d)" v x y);
+      if v = 0 then incr nulls_replaced;
+      let max_index = Palette.size palette - 1 in
+      let safe_v =
+        if v = 0 then 1
+        else if v > max_index then max_index
+        else v in
+      (Z.of_int safe_v, color_depth)
+    )
   in
-  if color_depth > max_allowed_color_depth then
-    failwith (Printf.sprintf
-      "Color depth %d exceeds max allowed for palette size %d (max: %d)"
-      color_depth palette_len max_allowed_color_depth);  
 
-  Printf.printf "Color depth: %d\n" color_depth;   (*Debug*)
-  Printf.printf "Pixels length: %d, expected: %d\n" (Bytes.length pixels) (width * height);
+  (* Packing before encoding *)
+  let flattened = Lzw.flatten_codes 8 pixels in
+  let compressed = Lzw.encode flattened color_depth in
 
-  Printf.printf "Palette size: %d, color depth: %d\n" (Array.length colors) color_depth;
-  let compressed = Lzw.encode pixels color_depth in
-  Printf.printf "Uncompressed size: %d, Compressed size: %d\n%!" (Bytes.length pixels) (Bytes.length compressed);
-  
   let image = Image.v
     (width, height)
     colors
     compressed
     color_depth
-    false
+    true
   in
-
-  let null_count = ref 0 in                   (*Debug*)
-  Bytes.iter (fun c -> if Char.code c = 0 then incr null_count) pixels;
-  Printf.printf "Number of null bytes in pixels: %d\n%!" !null_count;
-
-  if Bytes.length pixels <> width * height then                   (*Debug*)
-    failwith (Printf.sprintf "too few/many pixels: expected %d got %d" (width * height) (Bytes.length pixels));
 
   let gif = GIF.from_image image in
   let filename = now_string () ^ ".gif" in
