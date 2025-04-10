e1f6aa6d7df72daffbcad5b840eebe5c254cbabc
Vanisha1606 <vanishag1606@gmail.com>
2025-04-11 00:19:53 +0530

    Made few changes in base files

--- src/base.mli
+++ src/base.mli
@@ -9,21 +9,33 @@ module PlatformKey : module type of Keysdl
 module PlatformMouse : module type of Mousesdl
 (** A module that provides platform-specific mouse handling, based on the {!Mousesdl} module. *)
 
+(** Unified event type for both keyboard and mouse events. *)
+type event =
+  | KeyDown of Key.t           (** A key was pressed. *)
+  | KeyUp of Key.t             (** A key was released. *)
+  | MouseButtonDown of int     (** A mouse button was pressed (button code). *)
+  | MouseButtonUp of int       (** A mouse button was released (button code). *)
+  | MouseMotion of { x: int; y: int; xrel: int; yrel: int }  (** The mouse moved. *)
+  | MouseWheel of { x: int; y: int }                        (** The mouse wheel was scrolled. *)
+
 type input_state = {
   keys: KeyCodeSet.t;
+  events: event list;  (** Accumulated input events for the current frame. *)
   mouse: Mouse.t;
 }
 (** Represents the current state of user input, including:
     - [keys]: The set of currently pressed keys.
+    - [events]: A list of input events (keyboard and mouse) that occurred this frame.
     - [mouse]: The current state of the mouse, including position and button presses. *)
 
 type boot_func = Screen.t -> Framebuffer.t
 (** Function called once a start of run *)
 
 type tick_func = int -> Screen.t -> Framebuffer.t -> input_state -> Framebuffer.t
-(** Function called once a frame during run *)
+(** Function called once per frame during run *)
 
 type functional_tick_func = int -> Screen.t -> input_state -> Primitives.t list
+(** A functional-style tick function that returns a list of primitives *)
 
 val run: string -> boot_func option -> tick_func -> Screen.t -> unit
 (** [run title boot tick screen] Creates the runloop *)
