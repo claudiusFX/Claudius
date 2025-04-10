f0128af64fe711e6353f0f428ac2235bf4eb2afe
Shreya Pawaskar <shreya.pawaskar.main@gmail.com>
2025-04-10 20:45:04 +0000

    added test for palette too big case

--- test/test_screenshot.ml
+++ test/test_screenshot.ml
@@ -1,28 +1,40 @@
+open OUnit2
 open Claudius
-open Screenshot
 
-let () =
-  let width, height = 100, 100 in
-  let scale = 2 in 
+let width, height = 100, 100
+let scale = 2
 
-  let test_palette name palette =
+let test_palette name palette =
+  name >:: (fun _ ->
     let size = Palette.size palette in
     Printf.printf "Testing palette: %s (size = %d)\n%!" name size;
 
     (* Generate framebuffer with values clamped to [0, palette_size - 1] *)
     let fb = Framebuffer.init (width, height) (fun x y ->
       let raw = (x * y + x + y) in
-      let clamped = raw mod size in
-      assert (clamped >= 0 && clamped < size);
-      clamped
+      raw mod (Palette.size palette)
     ) in
 
     Framebuffer.set_dirty fb;
-    save_screenshot ~scale fb palette
-  in
+    let screen = Screen.create width height scale palette in
+    Screenshot.save_screenshot screen fb
+  )
 
+let test_palette_too_big _ =
+  let palette = Palette.generate_mono_palette 300 in          (* > 256 entries *) 
+  let fb = Framebuffer.init (width, height) (fun _ _ -> 42) in
+  Framebuffer.set_dirty fb;
+  let screen = Screen.create width height scale palette in
+  assert_raises
+    (Failure "GIF only supports up to 256 colors")
+    (fun () -> Screenshot.save_screenshot screen fb)
+
+let () =
+let suite = "screenshot_tests" >::: [
   test_palette "vapourwave" (Palette.generate_vapourwave_palette 64);
   test_palette "vga" (Palette.generate_microsoft_vga_palette ());
   test_palette "monopalette" (Palette.generate_mono_palette 256);
+  "raises error when palette exceeds 256 colors" >:: test_palette_too_big;        
+] in
 
-flush_all()
+run_test_tt_main suite;
