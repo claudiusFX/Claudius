f97aa237da12f0a18cb4046c6a58a08363b83415
Shreya Pawaskar <shreya.pawaskar.main@gmail.com>
2025-04-10 08:05:45 +0000

    documenting the scaling feature for img saving

--- src/screenshot.mli
+++ src/screenshot.mli
@@ -1,9 +1,10 @@
 (** Module for handling screenshots *)
 
-val save_screenshot : Framebuffer.t -> Palette.t -> unit
-(** [save_screenshot framebuffer palette] saves a screenshot of the current framebuffer
+val save_screenshot : scale:int -> Framebuffer.t -> Palette.t -> unit
+(** [save_screenshot ~scale framebuffer palette] saves a screenshot of the current framebuffer
     as a GIF file, using the given palette for colors. The screenshot is saved with a
-    timestamped filename like "screenshot_MMDDYY_HHMMSS.gif" for uniqueness. *)
+    timestamped filename like "screenshot_MMDDYY_HHMMSS.gif" for uniqueness. 
+    The output image is scaled by the given [scale] factor using nearest-neighbor scaling. *)
 
 val color_table_of_palette : Palette.t -> Giflib.ColorTable.t
 (** [color_table_of_palette palette] converts a Claudius palette into a Giflib color table,
