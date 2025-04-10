30d6b9f04172ec33cbb3a2f69b12a04ee7c2f94c
Shreya Pawaskar <shreya.pawaskar.main@gmail.com>
2025-04-10 20:43:45 +0000

    updated interface to reflect suggested changes

--- src/screenshot.mli
+++ src/screenshot.mli
@@ -1,11 +1,6 @@
 (** Module for handling screenshots *)
 
-val save_screenshot : scale:int -> Framebuffer.t -> Palette.t -> unit
-(** [save_screenshot ~scale framebuffer palette] saves a screenshot of the current framebuffer
-    as a GIF file, using the given palette for colors. The screenshot is saved with a
-    timestamped filename like "screenshot_MMDDYY_HHMMSS.gif" for uniqueness. 
-    The output image is scaled by the given [scale] factor using nearest-neighbor scaling. *)
-
-val color_table_of_palette : Palette.t -> Giflib.ColorTable.t
-(** [color_table_of_palette palette] converts a Claudius palette into a Giflib color table,
-    which is needed to write GIFs with correct colors. *)
+val save_screenshot : Screen.t -> Framebuffer.t -> unit
+(** [save_screenshot screen framebuffer] saves a screenshot of the current framebuffer.
+    The screenshot is saved with a timestamped filename like "screenshot_DDMMYY_HHMMSS.gif" for uniqueness. 
+    The output image is scaled by screen's scale factor factor using nearest-neighbor scaling. *)
