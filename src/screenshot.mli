8ef311d5cb19dffec35f921c7131db089b3a46a1
Shreya Pawaskar <shreya.pawaskar.main@gmail.com>
2025-04-11 15:30:34 +0000

    removed save_screenshot and added maybe_save_screenshot to mli

--- src/screenshot.mli
+++ src/screenshot.mli
@@ -1,6 +1,7 @@
 (** Module for handling screenshots *)
 
-val save_screenshot : Screen.t -> Framebuffer.t -> unit
-(** [save_screenshot screen framebuffer] saves a screenshot of the current framebuffer.
-    The screenshot is saved with a timestamped filename like "screenshot_DDMMYY_HHMMSS.gif" for uniqueness. 
-    The output image is scaled by screen's scale factor factor using nearest-neighbor scaling. *)
+val maybe_save_screenshot : Key.t list -> Screen.t -> Framebuffer.t -> unit
+(** [maybe_save_screenshot keys screen framebuffer] saves a screenshot  with a timestamped filename 
+    like "screenshot_DDMMYY_HHMMSS.gif" for uniqueness. The output image is scaled by screen's scale factor factor
+    Prevents multiple screenshots if F2 is held down. *)
+    
