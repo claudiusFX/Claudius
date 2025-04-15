48cad264b5f92faf2eee2ab1973ed52d243ed04f
essa237 <wahvanessa22@gmail.com>
2025-04-15 08:56:45 +0100

    Add unit test for run loop created a seperate module and interface to for mocking sdl

--- src/sdl_api.mli
+++ src/sdl_api.mli
@@ -1,28 +1,29 @@
 open Tsdl
 
 module type SDL_API = sig
-
   (*These types will represents abstract sdl object*)
-
   type window
-  type event
   type renderer
   type texture
+  type event
 
-  val init : int -> string -> bool -> (window * renderer, string) result
-  (**This handle the initialization of SDL
-    arguments width, height, window title and fullscreen flag and onsuccess, this returns a window and a renderer and on failure we have an error message
+  
+  val init : int -> int -> string -> bool -> (window * renderer, string) result
+   (**This handle the initialization of SDL
+    arguments width, height, window title and fullscreen flag and onsuccess, 
+    this returns a window and a renderer and on failure we have an error message
   *)
-
+  
   val create_texture : renderer -> Sdl.Pixel.format -> width:int -> height:int -> Sdl.Texture.access -> (texture, string) result
-  (* Create a texture, given a renderer, a pixel format and dimension of width and height if successful, a texture is returned else an error*)
-
-  val update_texture : renderer -> BigArray.Array1.t -> int -> (unit, string) result
+  (* Create a texture, given a renderer, a pixel format and dimension of width and
+   height if successful, a texture is returned else an error*)
+ 
+  val update_texture : renderer -> texture -> Bigarray.Array1.t -> int -> (unit, string) result
   (* Update texture with pixel data from BigArray the integer parameter, represents pixel width *)
 
   val renderer_clear : renderer -> (unit, string) result
   (* Clears renderer drawing target*)
-
+ 
   val renderer_copy : renderer -> texture -> Sdl.Rect.t option -> (unit, string) result
   (* Copies a texture to a renderer's target indicating where to place the texture*)
 
@@ -31,7 +32,7 @@ module type SDL_API = sig
 
   val poll_event : unit -> event option
   (* Polls an SDL event returns event if available*)
-  
+
   val delay : int -> unit
   (* delays execution for a number of milliseconds*)
 
