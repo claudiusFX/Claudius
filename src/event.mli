d0cf01ae2e5b142d7126ec8dcbc75e4cbca33ba9
Vanisha1606 <vanishag1606@gmail.com>
2025-04-19 18:23:52 +0530

    Made changes and added new files in src

---
+++ src/event.mli
@@ -0,0 +1,9 @@
+(* event.mli *)
+type t =
+  | KeyDown of Key.t
+  | KeyUp of Key.t
+  | MouseButtonDown of Mouse.button * (int * int)
+  | MouseButtonUp of Mouse.button * (int * int)
+  | MouseMotion of (int * int)
+  | MouseWheel of int
+  | MouseDrag of Mouse.button * (int * int)
--- src/mouse.mli
+++ src/mouse.mli
@@ -6,25 +6,11 @@ type button =
   | Right
   (** Represents a mouse button. *)
 
-type event =
-  | Button_down of button * (int * int)     (** Button press event with position *)
-  | Button_up of button * (int * int)       (** Button release event with position *)
-  | Motion of (int * int)                   (** Mouse movement event *)
-  | Wheel of int                            (** Scroll wheel event *)
-  | Drag of button * (int * int)            (** Drag event: button is held down while moving *)
-  (** Represents a mouse event. *)
-
 type t
 (** Abstract type representing the mouse state. *)
 
 val create : int -> t
-(** [create scale] initializes a new mouse state with a given scale factor after sanity check. *)
-
-val clear_events : t -> t
-(** [clear_event t] clears all stored mouse events. This is called every tick to prevent memory issues. *)
-
-val add_event : t -> event -> t
-(** [add_event t e] adds a new event [e] to the mouse state [t], applying coordinate scaling. *)
+(** [create scale] initializes a new mouse state with the given scale factor. *)
 
 val update_position : t -> (int * int) -> t
 (** [update_position t (x, y)] updates the mouse position to [(x / scale, y / scale)]. *)
@@ -38,5 +24,5 @@ val is_button_pressed : t -> button -> bool
 val get_position : t -> (int * int)
 (** [get_position t] returns the current mouse position. *)
 
-val get_events : t -> event list 
+val get_scale : t -> int 
 (** [get_events t] retrieves the list of recorded mouse events. Events are cleared every tick, so this must be called within the same tick they occur. *)
