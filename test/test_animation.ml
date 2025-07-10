3e0c41ca801a72f3eed2d7abb90aba16625170f4
Shreya Pawaskar <shreya.pawaskar.main@gmail.com>
2025-07-10 13:44:22 +0000

    modified tests

--- test/test_animation.ml
+++ test/test_animation.ml
@@ -5,54 +5,54 @@ let width, height = (100, 100)
 let scale = 2
 
 let test_basic_recording _ =
-  let recording_state = ref None in
   let palette = Palette.generate_vapourwave_palette 64 in
   let fb = Framebuffer.init (width, height) (fun x y -> x * y mod 64) in
   let screen = Screen.create width height scale palette in
+  let mut_state = ref (Some (Animation.start_recording 10)) in
 
-  Animation.start_recording recording_state 10;
+    for _ = 1 to 10 do
+      match !mut_state with
+      | Some st -> mut_state := Animation.record_frame st screen fb
+      | None -> ()
+    done;
 
-  for _ = 0 to 9 do
-    Animation.record_frame recording_state screen fb
-  done;
-
-  assert_raises (Failure "Not recording animation") (fun () ->
-      Animation.stop_recording recording_state)
+  assert_equal None !mut_state
 
 let test_invalid_frame_count _ =
-  let recording_state = ref None in
   assert_raises (Failure "Number of frames must be positive") (fun () ->
-      Animation.start_recording recording_state 0);
+      ignore (Animation.start_recording 0));
   let msg =
     Printf.sprintf "Maximum %d frames allowed" Animation.max_frames_default
   in
   assert_raises (Failure msg) (fun () ->
-      Animation.start_recording recording_state
-        (Animation.max_frames_default + 1))
+      ignore (Animation.start_recording (Animation.max_frames_default + 1)))
 
 let test_double_recording _ =
-  let recording_state = ref None in
   let palette = Palette.generate_vapourwave_palette 64 in
   let fb = Framebuffer.init (width, height) (fun x y -> x * y mod 64) in
   let screen = Screen.create width height scale palette in
+  let mut_state = ref (Some (Animation.start_recording 10)) in
 
-  Animation.start_recording recording_state 10;
-  Animation.record_frame recording_state screen fb;
+  mut_state := Animation.record_frame (Option.get !mut_state) screen fb;
+
   assert_raises (Failure "Already recording animation") (fun () ->
-      Animation.start_recording recording_state 10);
-  Animation.stop_recording recording_state
+      ignore (Animation.start_recording 10));
+
+  ignore (Animation.stop_recording (Option.get !mut_state));
+  mut_state := None
 
 let test_palette_too_big _ =
-  let recording_state = ref None in
   let palette = Palette.generate_mono_palette 300 in
   let fb = Framebuffer.init (width, height) (fun _ _ -> 42) in
   let screen = Screen.create width height scale palette in
 
-  Animation.start_recording recording_state 10;
+  let state = Animation.start_recording 10 in
+
   assert_raises (Failure "GIF only supports up to 256 colors") (fun () ->
-      Animation.record_frame recording_state screen fb);
-  assert_raises (Giflib.GIF.Error "from_images: empty image list") (fun () ->
-      Animation.stop_recording recording_state)
+      ignore (Animation.record_frame state screen fb));
+  assert_raises
+    (Giflib.GIF.Error "from_images: empty image list")
+    (fun () -> ignore (Animation.stop_recording state))
 
 let suite =
   "animation_tests"
