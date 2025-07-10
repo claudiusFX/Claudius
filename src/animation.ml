30a17c3062c275c188a1f0c12e0fee18a9cffa54
Shreya Pawaskar <shreya.pawaskar.main@gmail.com>
2025-07-10 13:42:59 +0000

    made more functional

--- src/animation.ml
+++ src/animation.ml
@@ -9,47 +9,35 @@ type recording_state_t = {
 
 let max_frames_default = 500
 
-let start_recording ?(max_frames = max_frames_default)
-    (recording_state : recording_state_t option ref) n =
-  match !recording_state with
-  | Some _ -> failwith "Already recording animation"
-  | None ->
+let start_recording ?(max_frames = max_frames_default) (n : int) : recording_state_t =
       if max_frames <= 0 then failwith "Number of frames must be positive";
       if n <= 0 then failwith "Number of frames must be positive";
       if n > max_frames then
         failwith (Printf.sprintf "Maximum %d frames allowed" max_frames_default);
-      recording_state :=
-        Some { frames = []; frames_to_record = n; current_frame = 0 };
-      Printf.printf "Started recording %d frames\n%!" n
+      Printf.printf "Started recording %d frames\n%!" n;
+      { frames = []; frames_to_record = n; current_frame = 0 }
 
-let stop_recording (recording_state : recording_state_t option ref) =
-  match !recording_state with
-  | None -> failwith "Not recording animation"
-  | Some rs ->
-      let frames = List.rev rs.frames in
+let stop_recording (recording_state : recording_state_t) : unit  =
+      let frames = List.rev recording_state.frames in
       let gif = GIF.from_images frames in
       let filename = timestamp "animation" ^ ".gif" in
       GIF.to_file gif filename;
-      Printf.printf "Animation saved as %s\n%!" filename;
-      recording_state := None
+      Printf.printf "Animation saved as %s\n%!" filename
 
-let record_frame (recording_state : recording_state_t option ref)
-    (screen : Screen.t) (fb : Framebuffer.t) =
-  match !recording_state with
-  | None -> ()
-  | Some rs ->
-      if rs.current_frame >= rs.frames_to_record then
-        stop_recording recording_state
-      else (
+let record_frame (recording_state : recording_state_t)
+    (screen : Screen.t) (fb : Framebuffer.t) : recording_state_t option =
         if Palette.size (Screen.palette screen) > 256 then
           failwith "GIF only supports up to 256 colors";
         let frame = capture_frame screen fb in
-        recording_state :=
-          Some
+        let updated_state =
             {
-              rs with
-              frames = frame :: rs.frames;
-              current_frame = rs.current_frame + 1;
-            };
-        if rs.current_frame + 1 = rs.frames_to_record then
-          stop_recording recording_state)
+              recording_state with
+              frames = frame :: recording_state.frames;
+              current_frame = recording_state.current_frame + 1;
+            }
+        in 
+        if updated_state.current_frame = updated_state.frames_to_record then (
+          stop_recording updated_state;
+          None 
+          ) else 
+          Some updated_state
